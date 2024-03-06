137d68e2b3b2287d375a3c706e2d70679ecdccc4
Kalana Thennakoon <kalanawijewardhana98@gmail.com>
2024-03-07 00:19:32 +0530

    first commit

---
+++ app/Views/dashboard.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+
+<title>Dashboard</title>
+<link  href="<?= base_url('Assests/css/bootstrap.min.css');?>" rel="stylesheet">
+
+</head>
+
+<body>
+
+<nav class="navbar fixed-top navbar-light bg-light">
+  <a class="navbar-brand" href="#">Fixed top</a>
+</nav>
+<script src="<?= base_url('Assests/js/jquery-3.7.1.js');?>" ></script>
+
+<script src="<?= base_url('Assests/js/bootstrap.min.js');?>" ></script>
+<script src="<?= base_url('Assests/js/popper.min.js');?>" ></script>
+
+</body>
+</html>
